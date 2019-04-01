7f33524c4680b4b5ed32266d5d88488561dc7261
Adam Hursey <adamhursey24@gmail.com>
2019-04-01 16:01:18 -0400

    started working with the store button

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+   myInput = React.createRef();
+   goToStore(event) {
+      event.preventDefault();
+      // Need to redirect page. Video 11
+
+   }
+
    render() {
       return (
-         <form className="store-selector">
+         <form className="store-selector" onSubmit={this.goToStore}>
             <h2>Pleast Enter A Store</h2>
-            <input type="text" required placeholder="Store Name" />
+            <input 
+               type="text" 
+               required placeholder="Store Name" 
+               ref={this.myInput}
+               defaultValue={getFunName()}
+            />
             <button type="submit">Visit Store</button>
          </form>
       )
